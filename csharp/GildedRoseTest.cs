8efc402f1dc045b1dbb18deb7d3d1031ff147099
Vaclav Stebra <vstebra@gmail.com>
2018-05-19 16:30:35 +0200

    Add tests for basic cases

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -14,5 +14,35 @@ namespace csharp
             app.UpdateQuality();
             Assert.AreEqual("foo", Items[0].Name);
         }
+
+        [Test]
+        public void WhenUpdatingQuality_SellInAndQualityAreDecreased()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 1 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(1, Items[0].SellIn);
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void WhenQualityIsZero_ItDoesNotDecrease()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 2, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(1, Items[0].SellIn);
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+
+        [Test]
+        public void WhenSellInHasPassed_QualityDecreasesTwiceAsMuch()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 2 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(-1, Items[0].SellIn);
+            Assert.AreEqual(0, Items[0].Quality);
+        }
     }
 }
